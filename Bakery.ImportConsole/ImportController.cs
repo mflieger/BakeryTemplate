f401c9561e1ad3544d98cf1bd9501d68102a7b80
Marcel Flieger <fliegermar@gmail.com>
2020-12-06 16:08:32 +0100

    Add project files.

---
+++ Bakery.ImportConsole/ImportController.cs
@@ -0,0 +1,14 @@
+﻿using Bakery.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.ImportConsole
+{
+  public class ImportController
+  {
+    public static IEnumerable<Product> ReadFromCsv()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
